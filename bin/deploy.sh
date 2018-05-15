345fa2823599b4620b3ef72931496958c662df88
Daniel Burnley <dan@danops.uk>
2018-05-15 22:00:28 +0100

    Attempt to fail if returns failure

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,6 @@
 rm -rf ./src/__pycache__
 zip function.zip ./src/*
 
-aws lambda update-function-code --region eu-west-1 --function-name "lunchRota" --zip-file fileb://function.zip
-aws lambda update-function-configuration --region eu-west-1 --function-name "lunchRota" --handler "src.lunch_rota.lambda_handler"
-
+aws lambda update-function-code --region eu-west-1 --function-name "lunchRota" --zip-file fileb://function.zip &&
+aws lambda update-function-configuration --region eu-west-1 --function-name "lunchRota" --handler "src.lunch_rota.lambda_handler" &&
 rm function.zip
