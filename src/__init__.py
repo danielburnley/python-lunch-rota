4dd6945e0649c22f738e8a2e46797fc24267ff33
Anthony King <anthony@madetech.com>
2018-08-03 15:49:36 +0100

    delete dead code

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .get_next_batch import GetNextBatch
-from .get_rota_for_year import GetRotaForYear
-from .team_member_gateway import TeamMemberGateway
-from .lunchers_gateway import LunchersGateway
-from .lambda_handler import LambdaHandler
